c144e8b365dc537b688b98fe7ce31ead8b9b6247
Alex <alex@ubuntu.ubuntu-domain>
2012-03-25 00:08:20 -0700

    work in progress

--- flood.js
+++ flood.js
@@ -5,23 +5,24 @@ var stream = fs.createWriteStream('output.txt', {'flags':'a'});
 var userinput = process.argv.slice(2);
 var numberofrequest =userinput[0];;
 var intervaltime= userinput[1];
-var latarray=[];
-var startarray=[];
-var badrequest=0;
-var latarrayindex=0;
-var total=0;
-var mean=0;
-var sd=0;
+//var latarray=[];
 
-//numberofrequest = userinput[0];
-//intervaltime= userinput[1];
-//console.log(userinput);
+//var startarray=[];
+//var badrequest=0;
+//var latarrayindex=0;
+//var total=0;
+//var mean=0;
+//var sd=0;
+//var globalStartTime = hrtime.time();
+//var reqPerSecond=0;
+var testresult;
+var resultarray= [];
 
 http.globalAgent.maxSockets=10000;
 
 var server = http.createServer(function(req, res){ 
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('HAHAHFHAHFHHHFH');
+  res.write('TEST PAGE');
   res.end();
 }).listen(8080);
 
@@ -45,17 +46,30 @@ var options3 = {
 };
 //testing LAN proxy
 var options4 = {  
-           host: '192.254.5.110',   
+           host: '142.58.63.37',   
            port: 8888,
 	   method: 'GET',
 path:'/fab'
 };
 
+function setup(){
+	testresult = {
+		numberofrequest =userinput[0];;
+		intervaltime= userinput[1];
+		latarray:[],
+		//var startarray=[];
+		badrequest:0,
+		total:0,
+		mean:0,
+		sd:0,
+		testStartTime: hrtime.time(),
+		reqPerSecond:0,	 
+		};
+};
 
 function test(n,t){
 
-	var counter = 0;
-	
+	var counter = 0;	
 	var interval = function(){
 		if(counter < n){
 			temp(counter, latarray);						
@@ -69,47 +83,35 @@ function test(n,t){
 };
 
 function testnowait(n){
-	for (var i =0; i< n; i++){
-			temp(i, latarray);
+	//for (var i =0; i< n; i++){
+	//		temp(i, latarray);
+	//};
+	var counter = 0;
+	while (counter < n){
+		temp(counter, latarray);
+		counter++;
 	};
 };
 
 function temp(k, array){
 	//'optionsX' for host option
+	var starttime ;
 	
 	var req = http.request(options1, function(res) {
-			startarray[k]=hrtime.time();
-		var starttime = hrtime.time();
-		//console.log('Request #',k,': sending http request to',options1.host,'at time =',starttime/1000000000,'seconds');
+			
+		var receivetime = hrtime.time();			
+		var latency = receivetime - req.starttime;
+		array.push(latency);
+		//console.log('Request #',k,': sending http request to',options1.host,'at time =',req.starttime/1000000000,'seconds');
 		
-		var timeRecorded=false;		
-		res.on('data', function (chunk) {
-		       	if(!timeRecorded){
-				var receivetime = hrtime.time();			
-				var latency = receivetime - starttime;	
-				
-				//console.log('Request #',k,': data received');
-				array[latarrayindex] = latency*1;
-				//console.log('Request #',k,': Latency =',array[latarrayindex],'nanoseconds' );
-				latarrayindex++;
-				//console.log('printint array', array);
-				//console.log('-------------DATA---------------');
-				//console.log(chunk);
-				timeRecorded=true;
-				if (array.length==numberofrequest-badrequest){
-					endoftest(array);
-					req.end();	
-						//res.emit('finish',endoftest(array));
-				};
+		res.on("end", function() {
+			if (array.length==numberofrequest-badrequest){
+				endoftest(array);	
+				//res.emit('finish',endoftest(array));
 			};
-			req.end();
-			//res.on('end', function() {
-			//	var endtime = hrtime.time();
-			//	var timeneeded = endtime - receivetime;						
-			//	console.log('Request #',k,': END OF DATA');
-			//	console.log('Request #',k,': data transfer time =',timeneeded,'nanoseconds');
-			//});
-		});	
+		})
+
+			
 	});
 	req.on('error', function(e) {
 		console.log('Request #',k,': ERROR',e.message);
@@ -120,17 +122,23 @@ function temp(k, array){
 		};
 	});
 	req.write('data\n');
-
+	req.on("drain", function() {
+		req.starttime = hrtime.time();
+	})
 	req.end();		
 };
 
 function endoftest(array){
 
-	console.log('END OF LATENCY TEST');
+	var testTook = hrtime.time() - testStartTime;
+
+	reqPerSecond = array.length / ((testTook/1000000)/1000);
+
+	console.log('END OF LATENCY TEST got '+reqPerSecond+" reqs/sec");
 	total = calTotal(array);
 	mean = calMean(total,array.length);
 	sd = calSD(array,mean);
-	console.log('Number of request = ',startarray.length);
+	console.log('Number of request = ',numberofrequest);
 	console.log('Total Latency = ',Math.round(total/1000000)/1000,'seconds');				
 	//console.log(total);
 	console.log('Mean = ',Math.round(mean/1000)/1000,'milliseconds');
@@ -138,26 +146,26 @@ function endoftest(array){
 	console.log('Standard Deviation = ',Math.round(sd/1000)/1000,'milliseconds');
 	//console.log(sd);
 	console.log('Number of bad request =',badrequest);
-	output();
+	output2();
 	
 	//testing start time
 
-	var temp=0;
-	startarray.sort();
+	//var temp=0;
+	//startarray.sort();
 	//console.log(startarray);
-	for (var i=0; i<startarray.length-1; i++){
+	//for (var i=0; i<startarray.length-1; i++){
 		//console.log(startarray[i]);
 		//console.log(startarray[i+1]);
 				
-		temp = temp+ ( startarray[i+1]*1-startarray[i]*1);
+	//	temp = temp+ ( startarray[i+1]*1-startarray[i]*1);
 		//console.log(temp);
 		//console.log(startarray[i]/1000000000);
 		//	console.log(temp);
-	};
+	//};
 	//console.log(numberofrequest,'  ',startarray.length);
 	//console.log(temp);
 	//console.log(numberofrequest);
-	console.log('Request sent at an average interval of '+(temp/(numberofrequest-1))/1000000000+' seconds');
+	
 };
 
 function output(){
@@ -187,6 +195,27 @@ function output(){
 	//process.exit();
 };
 
+function output2(){
+
+	//var options = {
+		//numberofrequest: numberofrequest [
+	//	testresult:{
+	//	numberofrequest: numberofrequest,	
+	//	total:total,
+	//	mean: mean,
+	//	sd: sd,
+	//	badrequest: badrequest
+	//	}
+	//};
+	//var data = JSON.stringify(options, null,2);
+	console.log('SUMMARY SAVED TO OUTPUT.TXT');
+	//stream.write('{\n');	
+	//stream.write('"testresult": {\n');
+	stream.write(Math.round(reqPerSecond)+'\n');	
+	stream.write((mean)+'\n');
+	stream.end();
+
+};
 
 function calTotal(array){
 	//console.log('running caltotal function');	
@@ -227,11 +256,59 @@ function calSD (array, m){
 	return temp;
 };
 
-if (userinput.length == 1){
-	//console.log('no wait');
-	testnowait(numberofrequest);
-}else { 
-test(numberofrequest,intervaltime);
-};
+function aaa(){
 
+	setup();
+};
+//if (userinput.length == 1){
+	//console.log('no wait');
+//	testnowait(numberofrequest);
+//}else { 
+//test(numberofrequest,intervaltime);
+//};
+//console.log(testresult.badrequest);
+setup();
+resultarray.push(testresult);
+testresult.badrequest = 100;
+setup();
+resultarray.push(testresult);
+testresult.badrequest = 110;
+setup();
+resultarray.push(testresult);
+setup();
+resultarray.push(testresult);
+setup();
+resultarray.push(testresult);
+setup()
+
+resultarray[0].badrequest=0;
+resultarray[1].badrequest=1;
+resultarray[2].badrequest=2;
+resultarray[3].badrequest=3;
+resultarray[4].badrequest=4;
+//testresult.badrequest = 100000000;
+console.log(resultarray[0].badrequest);
+console.log(resultarray[1].badrequest);
+console.log(resultarray[2].badrequest);
+console.log(resultarray[3].badrequest);
+console.log(resultarray[4].badrequest);
+console.log(testresult.badrequest);
+console.log('end of setup test');
+
+
+//console.log(testresult.badrequest);
+//resultarray.push(testresult);
+//console.log(resultarray[5].badrequest);
+testresult.badrequest = 10000;
+
+console.log(resultarray[0]);
+console.log(resultarray[1]);
+console.log(resultarray[2]);
+console.log(resultarray[3]);
+console.log(resultarray[4]);
+//testresult.badrequest = 100;
+console.log('testing after');
+//console.log(testresult.badrequest);
+//console.log(resultarray[0].badrequest);
+//console.log(resultarray[1].badrequest);
 
